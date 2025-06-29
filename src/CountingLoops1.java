4612a7b08efe27725311dcc5a72797e22d36e0a7
SueBugLLC <ramellsn@mail.uc.edu>
2025-06-29 17:08:20 -0400

    CountingLoops2

--- src/CountingLoops1.java
+++ src/CountingLoops1.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class CountingLoops1 {
+class CountingLoops {
+    public static void main(String[] args) {
+        // Task 1: Count up by 1 from 0 to 30
+        System.out.print("Task 1: ");
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // Adds a line break after the loop
+    }
 }
---
+++ src/CountingLoops2.java
@@ -0,0 +1,10 @@
+class CountingLoops2 {
+    public static void main(String[] args) {
+        // Task 2: Count down by 1 from 30 to 0
+        System.out.print("Task 2: ");
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
