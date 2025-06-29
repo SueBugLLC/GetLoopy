1fec029d0ee101abfe26f21fc7e562dd5c84ca8f
SueBugLLC <ramellsn@mail.uc.edu>
2025-06-29 17:20:44 -0400

    Finished

---
+++ src/CountingLoops3.java
@@ -0,0 +1,10 @@
+class Task3_CountUpByThree {
+    public static void main(String[] args) {
+        // Task 3: Count up by 3 from 0 to 18
+        System.out.print("Task 3: ");
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/CountingLoops4.java
@@ -0,0 +1,11 @@
+
+class Task4_CountDownByTwo {
+    public static void main(String[] args) {
+        // Task 4: Count down by 2 from 10 to 0
+        System.out.print("Task 4: ");
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
